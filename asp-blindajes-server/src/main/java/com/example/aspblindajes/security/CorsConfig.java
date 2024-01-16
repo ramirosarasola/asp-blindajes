f8ae7c7a07318b3ad0c797cd0811a4e91ee297de
ramirosarasola <ramiro.sarasola@gmail.com>
2024-01-16 10:57:15 -0300

    delete comments

--- asp-blindajes-server/src/main/java/com/example/aspblindajes/security/CorsConfig.java
+++ asp-blindajes-server/src/main/java/com/example/aspblindajes/security/CorsConfig.java
@@ -10,8 +10,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Map all endpoints
-                .allowedOrigins("*")    // Allow all origins
-                .allowedMethods("*");   // Allow all methods
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
     }
 }
